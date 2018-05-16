f9fa95f2dae29a8a1f7a0701efb7f6984ccee45b
dunn-cwu <andrew.dunn@cwu.edu>
2018-05-15 20:44:20 -0700

    Fixed namespace and added map resource

--- DungeonsAndDoodles/Program.cs
+++ DungeonsAndDoodles/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CS446_Project_LivePrototype
+namespace DungeonsAndDoodles
 {
     static class Program
     {
